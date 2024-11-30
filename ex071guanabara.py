d5aac1c75fe1e9c09af498f03954a41029c48584
CarlosCFonseca <carloscfonseca@gmail.com>
2024-11-30 11:14:40 -0300

    Arrumei a solução, agora esta correta

--- ex071guanabara.py
+++ ex071guanabara.py
@@ -7,14 +7,14 @@ while True:
         total -= ced
         totced += 1
     else:
-        if totced > 1:
+        if totced > 0:
             print(f'Total de {totced} cedulas de R${ced}')
         if ced ==50:
             ced = 20
         elif ced == 20:
             ced = 10
         elif ced == 10:
-            ced =1
+            ced = 1
         totced = 0
         if total == 0:
             break
